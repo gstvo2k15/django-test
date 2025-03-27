123d3bd7646ca4ef41e633f85afaabdfa7fc650d
Gustavo Olmo <gstvo2k15@gmail.com>
2025-03-27 18:32:46 +0100

    Fix dependencie issues-1d

--- frontend/src/tailwind.config.js
+++ frontend/src/tailwind.config.js
@@ -1,12 +1,10 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{vue,js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
-  
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
