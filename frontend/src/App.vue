c2548c0fca062f24702ccb34e412d0d416ac7a07
Gustavo Olmo <gstvo2k15@gmail.com>
2025-03-27 17:16:45 +0100

    Add initial files

---
+++ frontend/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Middleware Dashboard</h1>
+    <p>Tu panel está corriendo correctamente.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
