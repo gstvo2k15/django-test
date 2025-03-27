fd7698c27e350c5db9a0a145508e97ee9f3df1ed
Gustavo Olmo <gstvo2k15@gmail.com>
2025-03-27 18:27:43 +0100

    Fix dependencie issues-1c

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,11 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Dashboard from './views/Dashboard.vue'
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './assets/tailwind.css'
-
-createApp(App).use(router).mount('#app')
 
 const routes = [
   { path: '/', component: Dashboard },
