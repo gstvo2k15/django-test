9a1484df46636635db7e369d0e77704f19ddb042
Gustavo Olmo <gstvo2k15@gmail.com>
2025-03-28 13:04:05 +0100

    Include latest changes-3d

---
+++ core/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = "core"
+
+urlpatterns = [
+    path('', views.dashboard, name='dashboard'),
+    path('middleware/<slug:slug>/', views.middleware_detail, name='middleware_detail'),
+]
