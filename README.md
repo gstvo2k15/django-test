4f2266c8633115af4201fb7b5d7cc75b476f4d60
Gustavo Olmo <gstvo2k15@gmail.com>
2025-03-28 12:31:02 +0100

    Include latest changes-3c

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Middleware Dashboard
 
 Stack profesional para gestión de parches vía Ansible Tower.
+
+```
+docker compose exec backend python manage.py makemigrations core
+docker compose exec backend python manage.py migrate
+```
--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from core import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.dashboard, name='dashboard'),
+    path('', include(('core.urls', 'core'), namespace='core')),
 ]
